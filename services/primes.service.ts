0003567e870c885e7df18b8bf500a0ee7842b6c5
Ilya Yegorov <ilyayegorov@Ilyas-MacBook-Air.local>
2023-05-08 22:49:02 +0300

    primes service

---
+++ services/primes.service.ts
@@ -0,0 +1,23 @@
+import { catchError, from, map, Observable, take, throwError } from "rxjs";
+import axios, { AxiosError } from "axios";
+
+interface IPrimesResponse {
+  [key: string]: number;
+}
+
+export const PRIMES_URL =
+  "https://raw.githubusercontent.com/koorukuroo/Prime-Number-List/master/primes.json";
+
+export class PrimesService {
+  private readonly primesAmount = 150000;
+
+  public primes$: Observable<number[]> = from(
+    axios<IPrimesResponse>(PRIMES_URL)
+  ).pipe(
+    take(1),
+    map((response) =>
+      Array.from({ length: this.primesAmount }, (_, i) => response.data[i])
+    ),
+    catchError((err: AxiosError) => throwError(err.message))
+  );
+}
